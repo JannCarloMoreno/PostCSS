6ba80f7f4469965c606aeb2538f9669d47baac25
Jann <janncarlomoreno@gmail.com>
2019-03-23 18:35:41 -0500

    variables + calc

--- postcss/postcss.config.js
+++ postcss/postcss.config.js
@@ -1,9 +1,12 @@
 module.exports = {
 	plugins: [
+		require('postcss-apply'),
 		require('postcss-preset-env')({
 			autoprefixer: {
 				grid: true,
-			}
+				flexbox: false,
+			},
+			preserve: true,// valor por defecto es true
 		})
 	]
 }
