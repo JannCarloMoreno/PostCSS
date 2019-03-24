46d3fe67959d8b17cc9301f595aaa90adcaab63a
Jann <janncarlomoreno@gmail.com>
2019-03-24 16:09:37 -0500

    postcss-custom-media for easier media queries

--- postcss/postcss.config.js
+++ postcss/postcss.config.js
@@ -1,12 +1,16 @@
 module.exports = {
 	plugins: [
 		require('postcss-apply'),
+		require('postcss-custom-media')({
+			preserve: false,
+		}),
 		require('postcss-preset-env')({
 			autoprefixer: {
 				grid: true,
 				flexbox: false,
 			},
 			preserve: true,// valor por defecto es true
+			calc: false,
 		})
 	]
 }
